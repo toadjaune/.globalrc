ee4bc80a3874724406992fd40bcf8794ab4d64e5
Arnaud Venturi <arnaud.venturi@rez-gif.supelec.fr>
2016-02-08 23:48:41 +0100

    First version of the C code to detect unicode problems

---
+++ lib/wcinfo.c
@@ -0,0 +1,19 @@
+// Based on http://stackoverflow.com/a/23533623/1566221
+// compile with `gcc -std=c11 -Wall -o ../scripts/wcinfo wcinfo.c`
+//
+// This code tests if the first unicode character number should be displayed correctly
+// It is used to guess if we will have issues with unicode characters, testing the 2713 character
+
+#define _XOPEN_SOURCE 600
+#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <wctype.h>
+#include <wchar.h>
+
+int main(int argc, char** argv) {
+  setlocale(LC_CTYPE,"");
+  wint_t c = strtoul(argv[1], NULL, 16);
+  return wcwidth(c);
+}
+
