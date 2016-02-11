2e57668a91f4310c64ad4963390f071801207d54
Arnaud Venturi <arnaud.venturi@rez-gif.supelec.fr>
2016-02-11 02:37:27 +0100

    Began integration of automatic Unicode support testing

--- lib/unicode_test.c
+++ lib/unicode_test.c
@@ -1,5 +1,5 @@
 // Based on http://stackoverflow.com/a/23533623/1566221
-// compile with `gcc -std=c11 -Wall -o ../scripts/wcinfo wcinfo.c`
+// compile with `gcc -std=c11 -Wall -o ../scripts/unicode_test unicode_test.c`
 //
 // This code tests if the first unicode character number should be displayed correctly
 // It is used to guess if we will have issues with unicode characters, testing the 2713 character
@@ -14,6 +14,6 @@
 int main(int argc, char** argv) {
   setlocale(LC_CTYPE,"");
   wint_t c = strtoul(argv[1], NULL, 16);
-  return wcwidth(c);
+  return (wcwidth(c) != 1);
 }
 
