b9f67ebd96510c088c8c1d3b949a506bc79842c0
Arnaud Venturi <git@toadjaune.eu>
2023-07-05 11:50:34 +0200

    Cleanup some old obsolete aliases

--- roles/deploy/templates/bazshrc.sh
+++ roles/deploy/templates/bazshrc.sh
@@ -69,11 +69,8 @@ export SSH_AUTH_SOCK="${XDG_RUNTIME_DIR}/ssh-agent.socket"
 
 ### Begin aliases & alias functions ###
 
-# add colors to grep
+# add coloring
 alias grep='grep --color=auto'
-alias egrep='egrep --color=auto'
-alias fgrep='fgrep --color=auto'
-alias rgrep='rgrep --color=auto'
 
 # security aliases
 alias shred='shred -n 35 -z -u -v'
@@ -86,8 +83,6 @@ alias ....='cd ../../..'
 # Create a temporary directory and move to it
 alias cdtmp='pushd $(mktemp -d)'
 
-# History-related commands
-alias h='history'
 # This re-executes the last command, writes the output in a file descriptor, and opens it with vim,
 # thanks to process substitution
 # This syntax is compatible with both bash and zsh (unlike `fc -s`)
@@ -111,14 +106,6 @@ if command -v prettyping &>/dev/null; then
   alias ping=prettyping
 fi
 
-# maj
-if [ "$(which apt-get 2> /dev/null)" ]; then
-  alias maj='sudo apt-get update && sudo apt-get dist-upgrade'
-fi
-
-# 10 biggest files/directories in current directory
-alias topten='du -sk $(/bin/ls -A) | sort -rn | head -10'
-
 # tree from current directory
 alias tree="find . | sed 's/[^/]*\//|   /g;s/| *\([^| ]\)/+--- \1/'"
 
@@ -131,16 +118,10 @@ if command -v mosh >/dev/null 2>&1 ; then
   alias mosh="moshrc"
 fi
 
-# SSH without sshrc nor host key checking
-alias rescue_ssh="\ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
-
 # Way too long commands
 alias tf="terraform"
 alias tg="terragrunt"
 alias ku="kubectl"
-
-# Docker aliases
-alias stop_docker="docker ps | awk '{ print \$1 }' | grep -v '^CONTAINER' | xargs docker stop"
 
 # Imperial march on buzzer
 alias imperial-march="beep -l 350 -f 392 -D 100 --new -l 350 -f 392 -D 100 --new -l 350 -f 392 -D 100 --new -l 250 -f 311.1 -D 100 --new -l 25 -f 466.2 -D 100 --new -l 350 -f 392 -D 100 --new -l 250 -f 311.1 -D 100 --new -l 25 -f 466.2 -D 100 --new -l 700 -f 392 -D 100 --new -l 350 -f 587.32 -D 100 --new -l 350 -f 587.32 -D 100 --new -l 350 -f 587.32 -D 100 --new -l 250 -f 622.26 -D 100 --new -l 25 -f 466.2 -D 100 --new -l 350 -f 369.99 -D 100 --new -l 250 -f 311.1 -D 100 --new -l 25 -f 466.2 -D 100 --new -l 700 -f 392 -D 100 --new -l 350 -f 784 -D 100 --new -l 250 -f 392 -D 100 --new -l 25 -f 392 -D 100 --new -l 350 -f 784 -D 100 --new -l 250 -f 739.98 -D 100 --new -l 25 -f 698.46 -D 100 --new -l 25 -f 659.26 -D 100 --new -l 25 -f 622.26 -D 100 --new -l 50 -f 659.26 -D 400 --new -l 25 -f 415.3 -D 200 --new -l 350 -f 554.36 -D 100 --new -l 250 -f 523.25 -D 100 --new -l 25 -f 493.88 -D 100 --new -l 25 -f 466.16 -D 100 --new -l 25 -f 440 -D 100 --new -l 50 -f 466.16 -D 400 --new -l 25 -f 311.13 -D 200 --new -l 350 -f 369.99 -D 100 --new -l 250 -f 311.13 -D 100 --new -l 25 -f 392 -D 100 --new -l 350 -f 466.16 -D 100 --new -l 250 -f 392 -D 100 --new -l 25 -f 466.16 -D 100 --new -l 700 -f 587.32 -D 100 --new -l 350 -f 784 -D 100 --new -l 250 -f 392 -D 100 --new -l 25 -f 392 -D 100 --new -l 350 -f 784 -D 100 --new -l 250 -f 739.98 -D 100 --new -l 25 -f 698.46 -D 100 --new -l 25 -f 659.26 -D 100 --new -l 25 -f 622.26 -D 100 --new -l 50 -f 659.26 -D 400 --new -l 25 -f 415.3 -D 200 --new -l 350 -f 554.36 -D 100 --new -l 250 -f 523.25 -D 100 --new -l 25 -f 493.88 -D 100 --new -l 25 -f 466.16 -D 100 --new -l 25 -f 440 -D 100 --new -l 50 -f 466.16 -D 400 --new -l 25 -f 311.13 -D 200 --new -l 350 -f 392 -D 100 --new -l 250 -f 311.13 -D 100 --new -l 25 -f 466.16 -D 100 --new -l 300 -f 392.00 -D 150 --new -l 250 -f 311.13 -D 100 --new -l 25 -f 466.16 -D 100 --new -l 700 -f 392"
