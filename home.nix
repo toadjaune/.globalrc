5d7afd285fec5efd681b867d0349f659545ff360
Arnaud Venturi <git@toadjaune.eu>
2026-02-04 11:03:37 +0100

    Properly load custom vim configuration via home-manager

--- home.nix
+++ home.nix
@@ -108,6 +108,14 @@
   programs.eza.enable = true;
 
   # This defaults to installing vim-full, which we need for the wayland_clipboard option
-  programs.vim.enable = true;
+  programs.vim = {
+    enable = true;
+    # TODO: We should eventually move the rest of the configuration here, especially plugin install
+    # TODO: Variabilize path
+    extraConfig = ''
+      " Loading global vim configuration
+      source /home/toadjaune/.globalrc/vim/vimrc
+    '';
+  };
 
 }
