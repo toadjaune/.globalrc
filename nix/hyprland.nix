a38bbb2a923203f9d76fb5d57549059117df8b8d
Arnaud Venturi <git@toadjaune.eu>
2026-02-25 10:40:41 +0100

    Replace movetoworkspace with movetoworkspacesilent (don't follow window when moving it across workspaces)

--- nix/hyprland.nix
+++ nix/hyprland.nix
@@ -377,16 +377,16 @@
       bind = $mod, 0, workspace, 10
 
       # Move active window to a workspace with mod + SHIFT + [0-9]
-      bind = $mod SHIFT, 1, movetoworkspace, 1
-      bind = $mod SHIFT, 2, movetoworkspace, 2
-      bind = $mod SHIFT, 3, movetoworkspace, 3
-      bind = $mod SHIFT, 4, movetoworkspace, 4
-      bind = $mod SHIFT, 5, movetoworkspace, 5
-      bind = $mod SHIFT, 6, movetoworkspace, 6
-      bind = $mod SHIFT, 7, movetoworkspace, 7
-      bind = $mod SHIFT, 8, movetoworkspace, 8
-      bind = $mod SHIFT, 9, movetoworkspace, 9
-      bind = $mod SHIFT, 0, movetoworkspace, 10
+      bind = $mod SHIFT, 1, movetoworkspacesilent, 1
+      bind = $mod SHIFT, 2, movetoworkspacesilent, 2
+      bind = $mod SHIFT, 3, movetoworkspacesilent, 3
+      bind = $mod SHIFT, 4, movetoworkspacesilent, 4
+      bind = $mod SHIFT, 5, movetoworkspacesilent, 5
+      bind = $mod SHIFT, 6, movetoworkspacesilent, 6
+      bind = $mod SHIFT, 7, movetoworkspacesilent, 7
+      bind = $mod SHIFT, 8, movetoworkspacesilent, 8
+      bind = $mod SHIFT, 9, movetoworkspacesilent, 9
+      bind = $mod SHIFT, 0, movetoworkspacesilent, 10
 
       # Example special workspace (scratchpad)
       bind = $mod,        S, togglespecialworkspace,  magic
